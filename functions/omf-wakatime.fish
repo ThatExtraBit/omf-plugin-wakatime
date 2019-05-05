fd7b0d142b6d1c1495040993ca79c1e5d9eeb165
Sebastien Lambla <seb@serialseb.com>
2019-05-06 00:42:54 +0200

    Use last command and revparse

--- functions/omf-wakatime.fish
+++ functions/omf-wakatime.fish
@@ -1,13 +1,16 @@
-function wakatimeproj --on-event fish_postexec
-
-#  CMD_DURATION
+function _wt_current_directory
   if echo (pwd) | grep -qEi "^/Users/$USER/Dev/wf/dapi/"
-      set  project (echo (pwd) | sed "s#^/Users/$USER/Dev/wf/dapi/\\([^/]*\\).*#\\1#")
-  else if echo (pwd) | grep -qEi "^/Users/$USER/Dev/"
-      set  project (echo (pwd) | sed "s#^/Users/$USER/Dev/\\([^/]*\\).*#\\1#")
+      echo (pwd) | sed "s#^/Users/$USER/Dev/wf/dapi/\\([^/]*\\).*#\\1#"
   else
-      set project "Terminal"
+    echo (basename (git rev-parse --show-toplevel 2>/dev/null || echo "Terminal"))
   end
+end
 
-  wakatime --write --plugin "omf-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
+
+function _wt_last_command
+    echo $history[1] | cut -d ' ' -f1
+end
+
+function wt_heartbeet --on-event fish_postexec
+  wakatime --write --plugin "omf-wakatime/0.0.1" --entity-type app --project _wt_current_directory --entity _wt_last_command 2>&1 > /dev/null&
 end
