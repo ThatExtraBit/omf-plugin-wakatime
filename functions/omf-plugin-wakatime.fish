7b94cd847a66686ace21b5ed9d53379180cd841f
Sebastien Lambla <seb@serialseb.com>
2019-05-07 19:08:10 +0200

    Fix wakatime project

--- functions/omf-plugin-wakatime.fish
+++ functions/omf-plugin-wakatime.fish
@@ -10,7 +10,10 @@ end
 function _wt_last_command
     echo $history[1] | cut -d ' ' -f1
 end
-
+#
 function wt_heartbeet --on-event fish_postexec
-  wakatime --write --plugin "omf-wakatime/0.0.1" --entity-type app --project _wt_current_directory --entity _wt_last_command 2>&1 > /dev/null&
+  set --local current_dir (_wt_current_directory)
+  set --local last_cmd (_wt_last_command)
+  set --local fish_version (fish --version | cut -d ' ' -f3)
+  wakatime --write --plugin "Fish/$fish_version Fish-omf-wakatime-plugin/1.0.1" --category "building" --language "shell" --entity-type "app" --project "$current_dir" --entity "$last_cmd" 2>&1 > /dev/null&
 end
